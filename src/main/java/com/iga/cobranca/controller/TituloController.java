b205ab3da7187a713f2a8638d066be5e294c3754
raelAndrade <israelg.andrade@bol.com.br>
2020-01-28 23:31:31 -0300

    first commit

---
+++ src/main/java/com/iga/cobranca/controller/TituloController.java
@@ -0,0 +1,68 @@
+package com.iga.cobranca.controller;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.Errors;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.iga.cobranca.model.StatusTitulo;
+import com.iga.cobranca.model.Titulo;
+import com.iga.cobranca.repository.Titulos;
+
+@Controller
+@RequestMapping("/titulos")
+public class TituloController {
+	
+	private static final String CADASTRO_VIEW = "CadastroTitulo";
+	
+	@Autowired
+	private Titulos titulos;
+
+	@GetMapping("/novo")
+	public ModelAndView novo() {
+		ModelAndView mv = new ModelAndView(CADASTRO_VIEW);
+		mv.addObject(new Titulo());
+		return mv;
+	}
+	
+	@PostMapping
+	public String salvar(@Validated Titulo titulo, Errors errors, RedirectAttributes attributes) {
+		//System.out.println(">>>>" + titulo.getDescricao());
+		if (errors.hasErrors()) {
+			return CADASTRO_VIEW;
+		}		
+		titulos.save(titulo);
+		attributes.addFlashAttribute("mensagem", "Título salvo com sucesso!"); 
+		return "redirect:/titulos/novo";
+	}
+	
+	@GetMapping
+	public ModelAndView pesquisar() {
+		List<Titulo> todosTitulos = titulos.findAll();
+		ModelAndView mv = new ModelAndView("PesquisaTitulos");
+		mv.addObject("titulos", todosTitulos);
+		return mv;
+	}
+	
+	@GetMapping("{codigo}")
+	public ModelAndView edicao(@PathVariable("codigo") Titulo titulo) {
+		ModelAndView mv = new ModelAndView(CADASTRO_VIEW);
+		mv.addObject(titulo);
+		return mv;
+	}
+	
+	@ModelAttribute("todosStatusTitulo")
+	public List<StatusTitulo> todosStatusTitulo(){
+		return Arrays.asList(StatusTitulo.values());
+	}
+}
---
+++ src/main/java/com/iga/cobranca/repository/Titulos.java
@@ -0,0 +1,9 @@
+package com.iga.cobranca.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.iga.cobranca.model.Titulo;
+
+public interface Titulos extends JpaRepository<Titulo, Long> {
+
+}
